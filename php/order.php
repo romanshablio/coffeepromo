7880728fff9abcfe09bfa897858aa02d2d0a86d9
Роман Шаблий <roman@iMac-Roman.local>
2021-03-06 10:25:06 +0300

    added folders of project

---
+++ php/order.php
@@ -0,0 +1,49 @@
+<?php
+
+// получение данных с формы
+$Name = $_POST['Name'];
+$Phone = $_POST['Phone'];
+$PromoCode = $_POST['PromoCode'];
+//$Review = $_POST['Review'];
+
+//обработка и преобразование данных
+$Name = htmlspecialchars($Name); //преобразование символов в сущности
+$Phone= htmlspecialchars($Phone);
+$PromoCode= htmlspecialchars($PromoCode);
+//$Review= htmlspecialchars($Review);
+
+$Name = urldecode($Name); //декодирование URL
+$Phone= urldecode($Phone);
+$PromoCode= urldecode($PromoCode);
+//$Review= urldecode($Review);
+
+$Name = trim($Name); //удаление лишних пробелов
+$Phone= trim($Phone);
+$PromoCode= trim($PromoCode);
+//$Review= trim($Review);
+
+
+if (mail("romanshablio@icloud.com",
+            "Новый отзыв!",
+            "Имя: ".$Name."\n".
+            "Телефон: ".$Phone."\n".
+            "Промо-код: ".$PromoCode."\n",
+            //"Отзыв: ".$Review."\n",
+            "From: promo@hitryenot.ru \r\n")
+            ) {
+                echo ('Ваше сообщение успешно отправлено! Возвращайтесь за бесплатным кофе!');
+                
+            }
+            else {
+                echo('Есть ошибки, проверьте все ли обязательные поля заполнены.');
+            }
+?>
+<style>
+    *{
+      
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to left, #1366ff, #ff0b6d);
+
+    }
+</style>
